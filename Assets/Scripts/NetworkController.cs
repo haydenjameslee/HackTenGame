79c955c20bfdb157fe709f0ee29d0263f97af067
enderwigg <johnstark1989@gmail.com>
2015-05-02 13:57:05 -0500

    two players with cameras at either end

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkController : MonoBehaviour
+{
+	private string _room = "macrohard";
+	public static int _playerNum;
+
+	void Start ()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log ("joined lobby");
+
+		RoomOptions roomOptions = new RoomOptions() {};
+		PhotonNetwork.JoinOrCreateRoom(_room, roomOptions, TypedLobby.Default);
+	}
+	
+	void OnJoinedRoom()
+	{
+		_playerNum = PhotonNetwork.playerList.Length == 1 ? 1 : 2;
+		Debug.Log (_playerNum);
+
+		object[] photonData = new object[1];
+		photonData[0] = (object)_playerNum;
+
+		PhotonNetwork.Instantiate("PlayerController", Vector3.zero, Quaternion.identity, 0, photonData);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : Photon.MonoBehaviour
+{
+	private int _playerNum;
+	private GameObject _camera;
+
+	void Start ()
+	{
+		_playerNum = (int)photonView.instantiationData[0];
+		_camera = GameObject.Find ("Main Camera");
+
+		GameObject playerPosition = _playerNum == 1 ? GameObject.Find ("PlayerOnePosition") : GameObject.Find ("PlayerTwoPosition");
+		SetPlayerPosition(playerPosition);
+	}
+	
+	void Update ()
+	{
+	
+	}
+
+	void SetPlayerPosition(GameObject pos)
+	{
+		_camera.transform.position = pos.transform.position;
+		_camera.transform.rotation = pos.transform.rotation;
+		this.transform.SetParent(pos.transform);
+	}
+
+}
